af43f66a73e9b974ec4fe2c48659f0d2f8d4d036
AndresYudeh <e1315259398@live.uleam.edu.ec>
2022-04-21 12:08:02 -0500

    21-04-2022

--- Semana 2/clases1.js
+++ Semana 2/clases1.js
@@ -6,8 +6,22 @@ const persona={
         lat:234.234534,
         lng: 34.3453453,
     },
-    getNombreCompleto(){
-        return `${this.nombre}${this.apellido}}`;
-        console.log(persona.getNombreCompleto);
+    getNombreCompleto: function(){
+       return `${this.nombre} ${this.apellido}`;
+        
 }
 }
+console.log(persona.getNombreCompleto());
+
+//desestructurar funciones
+
+//function mostrarDatos({nombre, apellido, geolocalizacion:{lat, lng}, getNombreCompleto})
+
+//{
+   // console.log(nombre)
+   // console.log(apellido)
+   // console.log(lat)
+   // console.log(lng)
+   // console.log(getNombreCompleto)
+//}
+// mostrarDatos(persona)

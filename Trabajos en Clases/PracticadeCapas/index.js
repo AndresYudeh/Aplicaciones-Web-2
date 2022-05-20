f3b9761383f7cb63b3b76094db07469b1d4e881d
AndresYudeh <e1315259398@live.uleam.edu.ec>
2022-05-19 22:41:53 -0500

    19-05-2022

--- Trabajos en Clases/PracticadeCapas/index.js
+++ Trabajos en Clases/PracticadeCapas/index.js
@@ -9,5 +9,5 @@
 
 require('dotenv').config()
 const Server = require('./server');
-const server = new Server()
+const server = new Server();
 server.listen();
--- Trabajos en Clases/PracticadeCapas/middleware/validarcampos.js
+++ Trabajos en Clases/PracticadeCapas/middleware/validarcampos.js
@@ -1,13 +1,13 @@
 const {validationResult}= require('express-validator')
 
 const validarCampos = (req, res, next) =>{
-    const errors=validationResult(req)
-    if(errors.isEmpty()){
+    const errors=validationResult(req);
+    if(!errors.isEmpty()){
         res.status(400).json(errors);
     }
     next();
 }
 
 module.exports={
-    validarCampos
+    validarCampos,
 }

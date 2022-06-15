86c2c462461d049f85e5cdb7b243d4703ff1ca37
AndresYudeh <e1315259398@live.uleam.edu.ec>
2022-06-15 09:32:30 -0500

    15-06-2022

--- Trabajos en Clases/Semana 9/server.ts
+++ Trabajos en Clases/Semana 9/server.ts
@@ -1,9 +1,25 @@
-class Server{
-    constructor(){}
-    private async conectarDb(){}
-    private middlewares(){}
-    private routes(){}
-    listen(){}
-}
+// import {Router} from 'express'
 
-export{Server}
+// class Server{
+//     app: Router;
+//     router: Router;
+//     port: Number; 
+//     paths: {[key:string]:string}
+//     constructor(){
+//         this.app= Router();
+//         this.router= Router();
+//         this.port = Number {process.env.PORT};
+//         this.paths= {
+//             productos:'/api/productos',
+//             clientes:'/api/productos',
+//             prueba: 456,
+//         }
+//     }
+//     this.conectarDb{};
+//     private middlewares(){}
+//     private routes(){}
+//     listen(){}
+// }
+
+// export{Server}
+// //revisar bien TODO
